035efbf6c47858343e31ed404be0daf47fae0014
Marcos8060 <marcosgav80@gmail.com>
2022-04-27 22:05:58 +0300

    created the choose us section

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import './App.css';
 import Hero from './components/Hero';
 import Info from './components/Info';
 import Footer from './components/Footer';
-import Search from './components/Search';
+import Choose from './components/Choose';
 
 function App() {
   return (
     <div>
       <Hero />
       <Info />
-      <Search />
+      <Choose />
       <Footer />
     </div>
   );
