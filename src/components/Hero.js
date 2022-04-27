a70152adf06ab0529c5d059a64ad37bf205fcdbf
Marcos8060 <marcosgav80@gmail.com>
2022-04-27 16:14:59 +0300

    created the whole hero section

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './css/hero.css'
-import {Link} from 'react-router-dom'
+import car from '../images/range.webp'
 
 function Hero() {
   return (
@@ -18,9 +18,12 @@ function Hero() {
                         <a className='text-decoration-none btn' href="/login">Login</a>
                      </div>
                  </div>
+                 <h2 className='intro'>Hire the best cars in <br /> town at friendly prices.</h2>
+                 <p className='text'>Dont deny yourself the pleasure of driving the best premium <br /> cars from around the world here and now.</p>
+                 <p class="text">We have patnered with the world’s leading manufacture’s <br /> and brands to create a cross border experience <br /> without having to sacrifice the comfort of city rides.</p>
              </div>
               <div className='col-md-6 right'>
-                  right
+                  <img className='car' src={car} alt="" />
              </div>
          </div>
      </div>
