417b0201b6208ad1ed50527cfd60da6a9096b978
Marcos8060 <marcosgav80@gmail.com>
2022-04-27 15:44:33 +0300

    created the top left navbar section

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,9 +1,29 @@
 import React from 'react'
+import './css/hero.css'
+import {Link} from 'react-router-dom'
 
 function Hero() {
   return (
     <>
-      <h4>This is the hero sectio</h4>
+     <div className="app__hero">
+         <h6></h6>
+         <div className="row g-0">
+             <div className='col-md-6 left'>
+                 <div className="d-flex align-tems-center justify-content-between">
+                     <div>
+                       <h1 className='brand'>Range</h1>
+                     </div>
+                     <div>
+                        <a className='m-3 text-decoration-none btn' href="/register">Sign Up</a>
+                        <a className='text-decoration-none btn' href="/login">Login</a>
+                     </div>
+                 </div>
+             </div>
+              <div className='col-md-6 right'>
+                  right
+             </div>
+         </div>
+     </div>
     </>
   )
 }
