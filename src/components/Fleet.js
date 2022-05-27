859f168ce3d78b73de52e33012e2297d174b4595
Marcos8060 <marcosgav80@gmail.com>
2022-05-27 09:02:07 +0300

    added dynamic data to the fleet page

--- src/components/Fleet.js
+++ src/components/Fleet.js
@@ -1,68 +1,41 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import './css/fleet.css'
 import car from '../images/car1.jpg'
 import { Link } from 'react-router-dom'
 import LocalTaxiIcon from '@material-ui/icons/LocalTaxi';
 import DriveEtaIcon from '@material-ui/icons/DriveEta';
 import AirlineSeatReclineNormalIcon from '@material-ui/icons/AirlineSeatReclineNormal';
+import axios from 'axios'
 
+const url = 'http://127.0.0.1:8000/api/cars/'
 function Fleet() {
+    const [cars,setCars] = useState([])
+
+    useEffect(() =>{
+        axios.get(url)
+        .then((res) =>{
+            setCars(res.data)
+        })
+    },[])
   return (
     <div className='app__fleet'>
         <div className="container">
             <div className="row">
-                <div className="col-md-4" style={{ marginBottom: '7vh'}}>
+                {cars.map((car) =>(
+                    <div className="col-md-4" style={{ marginBottom: '7vh'}} key={car.id}>
                     <div>
-                        <img className='img-fluid fleetImg' src={car} alt="" />
+                        <img className='img-fluid fleetImg' src={car.image} alt="" />
                         <div className="footer">
-                          <p>Ferrari</p>
-                          <p className='price'>$ 23/day</p>
-                          <p className='gray'>Pieces of luggage: 4</p>
-                          <p className='gray'>Doors: 3</p>
-                          <p className='gray'>Maximum passengers: 4</p>
+                          <p>{car.name}</p>
+                          <p className='price'>$ {car.price}/day</p>
+                          <p className='gray'>Pieces of luggage: {car.luggage}</p>
+                          <p className='gray'>Doors: {car.doors}</p>
+                          <p className='gray'>Maximum passengers: {car.passengers}</p>
                           <Link to='/detail' className='btn viewBtn'>View Detail</Link>
                         </div>
                     </div>
                 </div>
-                <div className="col-md-4">
-                    <div>
-                        <img className='img-fluid fleetImg' src={car} alt="" />
-                        <div className="footer">
-                          <p>Ferrari</p>
-                          <p className='price'>$ 23/day</p>
-                          <p className='gray'>Pieces of luggage: 4</p>
-                          <p className='gray'>Doors: 3</p>
-                          <p className='gray'>Maximum passengers: 4</p>
-                          <Link to='/detail' className='btn viewBtn'>View Detail</Link>
-                        </div>
-                    </div>
-                </div>
-                <div className="col-md-4">
-                    <div>
-                        <img className='img-fluid fleetImg' src={car} alt="" />
-                        <div className="footer">
-                          <p>Ferrari</p>
-                          <p className='price'>$ 23/day</p>
-                          <p className='gray'>Pieces of luggage: 4</p>
-                          <p className='gray'>Doors: 3</p>
-                          <p className='gray'>Maximum passengers: 4</p>
-                          <Link to='/detail' className='btn viewBtn'>View Detail</Link>
-                        </div>
-                    </div>
-                </div>
-                <div className="col-md-4">
-                    <div>
-                        <img className='img-fluid fleetImg' src={car} alt="" />
-                        <div className="footer">
-                          <p>Ferrari</p>
-                          <p className='price'>$ 23/day</p>
-                          <p className='gray'>Pieces of luggage: 4</p>
-                          <p className='gray'>Doors: 3</p>
-                          <p className='gray'>Maximum passengers: 4</p>
-                          <Link to='/detail' className='btn viewBtn'>View Detail</Link>
-                        </div>
-                    </div>
-                </div>
+                ))}
             </div>
         </div>
     </div>
