e83e5c0ad97bc088795e3d7b13f3a85a9577d70f
Marcos8060 <marcosgav80@gmail.com>
2022-05-31 18:50:52 +0300

    created two reducer function

--- src/reducer.js
+++ src/reducer.js
@@ -1,5 +1,16 @@
 const reducer =(state,action) =>{
-
+    switch(action.type){
+        case 'ADD_TO_CART':
+            return{
+                ...state,
+                cart: [...state.cart,{...action.payload,qty:1}]
+            }
+        case 'REMOVE':
+            return{
+                ...state,
+                cart: state.cart.filter((x) => x.id !== action.payload.id)
+            }
+    }
     return state
 }
 
