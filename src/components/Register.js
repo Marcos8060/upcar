24e23473931c313490114a9efcf6d1763b1a6179
Marcos8060 <marcosgav80@gmail.com>
2022-06-01 02:44:57 +0300

    styled the register form

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./css/register.css";
+import { Link } from 'react-router-dom'
 
 function Register() {
   return (
@@ -8,6 +9,7 @@ function Register() {
         <div className="row registerContainer">
           <div className="col-md-5">
             <div className="card">
+                <h4 className="registerTitle">Sign Up</h4>
                 <form>
                     <label htmlFor="username" className="form-label">Username</label>
                     <input type="text" className="form-control" placeholder="username..." required />
@@ -15,7 +17,8 @@ function Register() {
                     <input type="email" className="form-control" placeholder="email..." required />
                     <label htmlFor="password" className="form-label">Password</label>
                     <input type="password" className="form-control" placeholder="password..." />
-                    <button className="registerBtn">Submit</button>
+                    <Link to='/login'>Already have an account?</Link>
+                    <button className="registerBtn mt-4">Submit</button>
                 </form>
             </div>
           </div>
