5b942326470e61d9bb41a80bc0e4bf00e89644e0
Marcos8060 <marcosgav80@gmail.com>
2022-06-10 01:50:16 +0300

    added dynamic form data to the summary section

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -3,64 +3,84 @@ import { CgProfile } from "react-icons/cg";
 import "./css/summary.css";
 import { GlobalContext } from "../context";
 import car from "../images/posh.jpg";
+import { Link } from "react-router-dom";
 
 function Summary() {
-  const {
-    state: { cart },
-  } = GlobalContext();
+  const { cart, finalData } = GlobalContext();
   return (
     <div className="container app__summary">
       <div className="card">
         <div className="row">
-          <div className="col-md-3 text-center leftSummary">
-            <CgProfile />
-            <br />
-            Username :  Marcos
-            <hr />
-            Number of Items : 2
-          </div>
-          <div className="col-md-6 centerSummary">
+          <div className="col-md-3 leftSummary">
             <h3>Dashboard</h3>
-            <div className="d-flex align-items-center justify-content-between">
-              <div className="d-flex align-items-center">
-                <div>
-                  <img className="img-fluid summaryImg" src={car} alt="" />
-                </div>
-                <div className="d-flex flex-column m-3">
-                  <p>Brand: Mercedes</p>
-                  <p>Price: $ 40</p>
-                  <p>Quantity: 4</p>
-                </div>
-              </div>
-              <div>
-                <p>Pickup Date : Today</p>
-                <p>DropOff Date : Today</p>
-                <p>Location : Nairobi</p>
-              </div>
-            </div>
+            <br />
+            <p>Username : Marcos</p>
+            {finalData.map((data) => (
+              <>
+                <p>First Name : {data.firstname}</p>
+                <p>Last Name : {data.lastname}</p>
+                <hr />
+                <p>Email : {data.email}</p>
+                <p>Address : {data.code}</p>
+                <p>County : {data.city}</p>
+                <p>Pickup Date : {data.pickup}</p>
+                <p>DropOff Date: {data.dropoff}</p>
+                <hr />
+                <small>
+                  Instructions: {data.instructions}
+                </small>
+              </>
+            ))}
             <hr />
-            <div className="d-flex align-items-center justify-content-between">
-              <div className="d-flex align-items-center">
-                <div>
-                  <img className="img-fluid summaryImg" src={car} alt="" />
-                </div>
-                <div className="d-flex flex-column m-3">
-                  <p>Brand: Mercedes</p>
-                  <p>Price: $ 40</p>
-                  <p>Quantity: 4</p>
-                </div>
-              </div>
-              <div>
-                <p>Pickup Date : Today</p>
-                <p>DropOff Date : Today</p>
-                <p>Location : Nairobi</p>
-              </div>
-            </div>
-            <hr />
+            <Link to="/payment" className=" btn payment">
+              Proceed to Payment
+            </Link>
           </div>
-          <div className="col-md-3">
-              <h4>Personal Information</h4>
-              <hr />
+          <div className="col-md-9 centerSummary">
+            <div className="row">
+              <div className="col-md-6">
+                <div className="row">
+                  <div className="col-md-6 text-center">
+                    <img className="img-fluid summaryImg" src={car} alt="" />
+                  </div>
+                  <div className="col-md-6 text-center">
+                    <p>Brand: Mercedes</p>
+                    <p>Price: $ 40</p>
+                    <p>Quantity: 4</p>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-6 text-center">
+                <p>Luggage : 4</p>
+                <p>Doors : 4</p>
+                <p>Passengers : 4</p>
+              </div>
+            </div>
+            <hr />
+            <div className="row">
+              <div className="col-md-6">
+                <div className="row">
+                  <div className="col-md-6 text-center">
+                    <img className="img-fluid summaryImg" src={car} alt="" />
+                  </div>
+                  <div className="col-md-6 text-center">
+                    <p>Brand: Mercedes</p>
+                    <p>Price: $ 40</p>
+                    <p>Quantity: 4</p>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-6 text-center">
+                <p>Luggage : 4</p>
+                <p>Doors : 4</p>
+                <p>Passengers : 4</p>
+              </div>
+            </div>
+            <hr />
+            <div className="d-flex justify-content-around align-items-center">
+              <h4>Total</h4>
+              <h4>$ 3000</h4>
+            </div>
           </div>
         </div>
       </div>
