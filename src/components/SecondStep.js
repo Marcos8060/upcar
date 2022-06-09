25fb3b7abf9d76c955f194340880562ef469cace
Marcos8060 <marcosgav80@gmail.com>
2022-06-09 22:24:09 +0300

    installed react icons

--- src/components/SecondStep.js
+++ src/components/SecondStep.js
@@ -1,6 +1,9 @@
 import React from 'react'
+import { GlobalContext } from '../context';
+import ArrowLeftIcon from '@material-ui/icons/ArrowLeft';
 
 function SecondStep() {
+    const { setCurrentStep, userData,setUserData} = GlobalContext();
   return (
     <div>
         <label htmlFor="address" className="form-label">Address</label>
@@ -9,6 +12,10 @@ function SecondStep() {
         <input type="text" className="form-control" placeholder='city...' />
         <label htmlFor="address" className="form-label">Postal Code</label>
         <input type="text" className="form-control" placeholder='code...' />
+        <div className='d-flex align-items-center justify-content-center'>
+        <button onClick={()=> setCurrentStep(3)} className='step2Btn'><ArrowLeftIcon/>Back</button>
+        <button onClick={()=> setCurrentStep(3)} className='step2Btn'>Next</button>
+        </div>
     </div>
   )
 }
